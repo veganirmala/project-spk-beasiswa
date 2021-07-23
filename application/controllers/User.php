cd229680bd75193e2031dd2af20834e9eaca3777
veganirmala <veganirmala31@gmail.com>
2021-07-23 09:35:54 +0800

    update index dan footer

--- application/controllers/User.php
+++ application/controllers/User.php
@@ -22,7 +22,7 @@ class User extends CI_Controller
         $data['user'] = $this->User_model->getUser();
 
         $this->load->view('template/header', $data);
-        $this->load->view('template/sidebar');
+        $this->load->view('template/sidebar', $data);
         $this->load->view('template/topbar', $data);
         $this->load->view('user/index', $data);
         $this->load->view('template/footer');
